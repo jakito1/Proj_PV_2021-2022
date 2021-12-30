568e7ffaee1b3d0270b19c8330feda5060c2233f
jakito1 <23082107+jakito1@users.noreply.github.com>
2021-12-30 17:42:34 +0000

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NutriFitWeb.Data
+***REMOVED***
+    public class ApplicationDbContext : IdentityDbContext
+    ***REMOVED***
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutriFitWeb.Models
+***REMOVED***
+    public class ErrorViewModel
+    ***REMOVED***
+        public string? RequestId ***REMOVED*** get; set; ***REMOVED***
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+***REMOVED***
+***REMOVED***
