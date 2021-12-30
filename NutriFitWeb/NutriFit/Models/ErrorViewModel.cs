3bb724693179fc411e98c5f532f60a2a6c59ad6e
jakito1 <23082107+jakito1@users.noreply.github.com>
2021-12-30 17:24:44 +0000

    First Commit

---
+++ NutriFitWeb/NutriFit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutriFit.Models
+***REMOVED***
+    public class ErrorViewModel
+    ***REMOVED***
+        public string? RequestId ***REMOVED*** get; set; ***REMOVED***
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+***REMOVED***
+***REMOVED***
