dd8cfa60d60736fcd32a5bb16432afb75550f8ef
Francisco Moura <201802033@estudantes.ips.pt>
2022-03-14 15:08:33 +0000

    Inicio Admin

---
+++ Controllers/AdminsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NutriFitWeb.Data;
+
+namespace NutriFitWeb.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AdminsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "administrator")]
+        public async Task<IActionResult> ShowAllUsers()
+        {
+            return View(_context.Users);
+        }
+
+        public async Task<IActionResult> DeleteUserAccount(int? id)
+        {
+
+        }
+    }
+}
