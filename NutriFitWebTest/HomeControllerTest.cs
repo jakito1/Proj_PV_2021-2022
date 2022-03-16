87efdbed1a40d36799676fbcac17052dabf35b5b
Luís Carvalho <32613648+LuisMLC@users.noreply.github.com>
2022-03-16 22:51:15 +0000

    Added Error view Test for HomeController

--- NutriFitWebTest/HomeControllerTest.cs
+++ NutriFitWebTest/HomeControllerTest.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
 using NutriFitWeb.Controllers;
+using NutriFitWeb.Models;
 using Xunit;
 
 namespace NutriFitWebTest
@@ -27,6 +31,23 @@ namespace NutriFitWebTest
             var result = controller.Privacy();
 
             var viewResult = Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Error_ReturnsViewResult()
+        {
+            var controller = new HomeController(new NullLogger<HomeController>(), null, null, null);
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.Setup(h => h.TraceIdentifier).Returns("Test");
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = mockHttpContext.Object;
+
+            var result = controller.Error();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<ErrorViewModel>(
+                viewResult.ViewData.Model);
+            Assert.NotNull(model);
         }
     }
 }
