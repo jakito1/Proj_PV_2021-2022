be50dc2b4d71d727c0511b3608f669b59e78f823
Francisco Moura <201802033@estudantes.ips.pt>
2022-03-25 22:34:10 +0000

    Inicio modelo plano de alimentação e otimização do modelo do exercicio

---
+++ Models/Meal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutriFitWeb.Models
+***REMOVED***
+    public class Meal
+    ***REMOVED***
+        public int MealId ***REMOVED*** get; set; ***REMOVED***
+        [Required]
+        public string? MealName ***REMOVED*** get; set; ***REMOVED***
+        public string? MealDescription ***REMOVED*** get; set; ***REMOVED***
+        public int? MealCalories ***REMOVED*** get; set; ***REMOVED***
+
+        [DataType(DataType.Time)]
+        public TimeSpan? MealTime ***REMOVED*** get; set; ***REMOVED***
+***REMOVED***
+***REMOVED***
---
+++ Models/NutritionPlan.cs
@@ -0,0 +1,6 @@
+﻿namespace NutriFitWeb.Models
+***REMOVED***
+    public class NutritionPlan
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
