ff9d28a4b02bfe614824ae183e207fabf8c7d21d
Francisco Moura <201802033@estudantes.ips.pt>
2022-03-25 22:34:10 +0000

    Inicio modelo plano de alimentação e otimização do modelo do exercicio

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -11,12 +11,19 @@ namespace NutriFitWeb.Models
         [Required(ErrorMessage = "Campo Obrigatório")]
         public string? ExerciseName { get; set; }
         public string? ExerciseDescription { get; set; }
-        [DataType(DataType.Time)]
-        public TimeSpan? ExerciseDuration { get; set; }
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Este campo tem de ser maior que {1}.")]
+        public int? ExerciseDuration { get; set; }
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Este campo tem de ser maior que {1}.")]
         public int? ExerciseRepetitions { get; set; }
         public string? ExerciseURL { get; set; }
         public List<Picture>? Pictures { get; set; }
+
+        [Column(TypeName = "nvarchar(24)")]
         public ExerciseType? ExerciseType { get; set; }
+
+        [Column(TypeName = "nvarchar(24)")]
         public ExerciseMuscles? ExerciseMuscles { get; set; }
 
         [JsonIgnore]
---
+++ Models/Meal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutriFitWeb.Models
+{
+    public class Meal
+    {
+        public int MealId { get; set; }
+        [Required]
+        public string? MealName { get; set; }
+        public string? MealDescription { get; set; }
+        public int? MealCalories { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeSpan? MealTime { get; set; }
+    }
+}
