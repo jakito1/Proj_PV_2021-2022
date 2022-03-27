0b7736e03d9815353d379d8276a509b3d0b8e07b
Francisco Moura <201802033@estudantes.ips.pt>
2022-03-27 11:47:42 +0100

    Minify js and css (site.js and site.css)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,20 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function limitExerciseRepetitions(input) ***REMOVED***
+    if (Math.abs(input.value) > 0) ***REMOVED***
+        input.value = Math.abs(input.value);
+***REMOVED*** else ***REMOVED***
+        input.value = null;
+***REMOVED***
+    if (Math.abs(input.value) > 999) ***REMOVED***
+        input.value = 999
+***REMOVED***
+***REMOVED***
+function limitExerciseDuration(input) ***REMOVED***
+    if (Math.abs(input.value) > 0) ***REMOVED***
+        input.value = Math.abs(input.value);
+***REMOVED*** else ***REMOVED***
+        input.value = null;
+***REMOVED***
+    if (Math.abs(input.value) > 120) ***REMOVED***
+        input.value = 120
+***REMOVED***
+***REMOVED***
