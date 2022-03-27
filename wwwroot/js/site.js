42a41223e35e990c3643bf47dff9d1b967526315
Francisco Moura <201802033@estudantes.ips.pt>
2022-03-27 12:07:47 +0100

    Adicionado ajax ao ficheiro de javascript para reduzir load times

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿function limitExerciseRepetitions(input) ***REMOVED***
+﻿function UpdateExercisesList() ***REMOVED***
+    $.ajax(***REMOVED***
+        url: "/Exercises/ShowExercisesList",
+        type: "GET",
+        datatype: "HTML",
+        success: function (data) ***REMOVED***
+            $('#_CreateExercisePartialForm').removeData('validator');
+            $('#_CreateExercisePartialForm').removeData('unobtrusiveValidation');
+            $.validator.unobtrusive.parse('#_CreateExercisePartialForm');
+            $("#_ShowExercisesPartialList").html(data);
+    ***REMOVED***
+***REMOVED***)
+***REMOVED***
+function UpdateCreateExercise() ***REMOVED***
+    $.ajax(***REMOVED***
+        url: "/Exercises/GetCleanCreateExercisePartial",
+        type: "GET",
+        datatype: "HTML",
+        success: function (data) ***REMOVED***
+            $("#_CreateExercisePartialItem").html(data);
+    ***REMOVED***
+***REMOVED***)
+***REMOVED***
+
+function limitExerciseRepetitions(input) ***REMOVED***
     if (Math.abs(input.value) > 0) ***REMOVED***
         input.value = Math.abs(input.value);
 ***REMOVED*** else ***REMOVED***
