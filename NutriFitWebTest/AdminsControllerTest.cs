44a7a7b369213a556e9d043a1e0150347b86b31c
Luís Carvalho <32613648+LuisMLC@users.noreply.github.com>
2022-04-03 10:58:58 +0100

    AdminsController general DB connection

--- NutriFitWebTest/AdminsControllerTest.cs
+++ NutriFitWebTest/AdminsControllerTest.cs
@@ -3,61 +3,77 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using NutriFitWeb.Controllers;
 using NutriFitWeb.Data;
-using System;
-using System.Diagnostics;
+using NutriFitWeb.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace NutriFitWebTest
 {
-    public class AdminsControllerTest
+    public class NutrifitContextFixture
     {
-        [Fact]
-        public async Task ShowAllUsers_ReturnsViewResult()
+        public ApplicationDbContext DbContext { get; private set; }
+
+        public NutrifitContextFixture()
         {
             var connection = new SqliteConnection("Datasource=:memory:");
             connection.Open();
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseSqlite(connection)
                 .Options;
+            DbContext = new ApplicationDbContext(options);
 
-            using (var context = new ApplicationDbContext(options))
-            {
-                context.Database.EnsureCreated();
-            }
+            DbContext.Database.EnsureCreated();
+        }
+    }
+    public class AdminsControllerTest : IClassFixture<NutrifitContextFixture>
+    {
+        private ApplicationDbContext _context;
 
-            using (var context = new ApplicationDbContext(options))
-            {
-                var controller = new AdminsController(context);
+        public AdminsControllerTest(NutrifitContextFixture contextFixture)
+        {
+            _context = contextFixture.DbContext;
+        }
+
+        [Fact]
+        public async Task ShowAllUsers_ReturnsViewResult()
+        {
+                var controller = new AdminsController(_context);
 
                 var result = await controller.ShowAllUsers(null);
 
                 Assert.IsType<ViewResult>(result);
-            }
         }
 
         [Fact]
         public async Task DeleteUserAccount_ReturnsNotFound_WhenAccontDoesntExist()
         {
-            var connection = new SqliteConnection("Datasource=:memory:");
-            connection.Open();
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseSqlite(connection)
-                .Options;
-
-            using (var context = new ApplicationDbContext(options))
-            {
-                context.Database.EnsureCreated();
-            }
-
-            using (var context = new ApplicationDbContext(options))
-            {
-                var controller = new AdminsController(context);
+                var controller = new AdminsController(_context);
 
                 var result = await controller.DeleteUserAccount(null, null);
 
                 Assert.IsType<NotFoundResult>(result);
-            }
+        }
+
+        [Fact]
+        public async Task DeleteUserAccount_ReturnsLocalUrl()
+        {
+            var controller = new AdminsController(_context);
+            Trainer mockTrainer = new()
+            {
+                TrainerId = 1,
+                TrainerFirstName = "Luis",
+                TrainerLastName = "Carvalho",
+                Gym = null,
+                UserAccountModel = new UserAccountModel { UserName = "trainer", Email = "trainer@trainer.pt", EmailConfirmed = true },
+                Clients = null,
+            };
+
+            string? trainerId = mockTrainer.UserAccountModel.Id;
+
+             _context.Trainer?.Add(mockTrainer);
+
+            var result = await controller.DeleteUserAccount(trainerId, "~/home");
         }
     }
 }
