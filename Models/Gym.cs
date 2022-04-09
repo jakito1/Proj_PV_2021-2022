814b7d72e40cb396d0ec7bf4456c10f6d3b3467c
Luís Carvalho <201702393@estudantes.ips.pt>
2022-04-09 20:59:49 +0100

    More Documentation

--- Models/Gym.cs
+++ Models/Gym.cs
@@ -2,22 +2,52 @@
 
 namespace NutriFitWeb.Models
 ***REMOVED***
+    /// <summary>
+    /// Gym class
+    /// </summary>
     public class Gym
     ***REMOVED***
+        /// <summary>
+        /// Gets and Sets the gym id.
+        /// Display name = ID do Ginásio
+        /// </summary>
         [DisplayName("ID do Ginásio")]
         public int GymId ***REMOVED*** get; set; ***REMOVED***
 
+        /// <summary>
+        /// Gets and Sets the gym name.
+        /// Display name = Nome
+        /// </summary>
         [DisplayName("Nome")]
         public string? GymName ***REMOVED*** get; set; ***REMOVED***
-
+        
+        /// <summary>
+        /// Gets and Sets the gym profile picture.
+        /// Display name = Foto de Perfil
+        /// </summary>
         [DisplayName("Foto de Perfil")]
         public Photo? GymProfilePhoto ***REMOVED*** get; set; ***REMOVED***
 
+        /// <summary>
+        /// Gets and Sets the gym user account model.
+        /// </summary>
         public UserAccountModel? UserAccountModel ***REMOVED*** get; set; ***REMOVED***
 
+        /// <summary>
+        /// Gets and Sets the gym client list.
+        /// </summary>
         public List<Client>? Clients ***REMOVED*** get; set; ***REMOVED***
+        /// <summary>
+        /// Gets and Sets the gym nutritionist list.
+        /// </summary>
         public List<Nutritionist>? Nutritionists ***REMOVED*** get; set; ***REMOVED***
+        /// <summary>
+        /// Gets and Sets the gym trainer list.
+        /// </summary>
         public List<Trainer>? Trainers ***REMOVED*** get; set; ***REMOVED***
+        /// <summary>
+        /// Gets and Sets the gym machines list.
+        /// </summary>
         public List<Machine>? Machines ***REMOVED*** get; set; ***REMOVED***
 
 ***REMOVED***
