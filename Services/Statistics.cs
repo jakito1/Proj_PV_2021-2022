2b4f877a1fab0493a1587afa3930c24123bd7d67
TiagoFtnt99 <62247332+TiagoFtnt99@users.noreply.github.com>
2022-04-13 18:37:50 +0100

    statistics of trainer v0 and minor fixes

--- Services/Statistics.cs
+++ Services/Statistics.cs
@@ -71,7 +71,8 @@ namespace NutriFitWeb.Services
             if (client is not null && client.Weight is not null && client.Weight > 0 &&
                     client.Height is not null && client.Height > 0)
             ***REMOVED***
-                return (double)(client.Weight / (client.Height * client.Height));
+                double tempHeight = (double)client.Height / 100;
+                return (double)(client.Weight / (tempHeight * tempHeight));
         ***REMOVED***
             return 0;
     ***REMOVED***
