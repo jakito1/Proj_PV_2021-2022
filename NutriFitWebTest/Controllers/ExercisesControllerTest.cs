6ecb5aceda38a211e43f92d6e7146d7c1b849209
Luís Carvalho <32613648+LuisMLC@users.noreply.github.com>
2022-04-15 18:04:50 +0100

    Added 2 tests do ExercisesController

--- NutriFitWebTest/Controllers/ExercisesControllerTest.cs
+++ NutriFitWebTest/Controllers/ExercisesControllerTest.cs
@@ -14,7 +14,7 @@ namespace NutriFitWebTest.Controllers
     ***REMOVED***
         private readonly HttpContext _httpContext;
         private readonly IPhotoManagement photoManagement;
-        private readonly List<Exercise> exercises = new List<Exercise>();
+        private readonly List<Exercise> exercises = null;
 
         public ExercisesControllerTest()
         ***REMOVED***
@@ -23,6 +23,7 @@ namespace NutriFitWebTest.Controllers
             mockSession["_Exercises"] = exercises;
             mockHttpContext.Setup(s => s.Session).Returns(mockSession);
             _httpContext = mockHttpContext.Object;
+            photoManagement = Mock.Of<IPhotoManagement>();
     ***REMOVED***
 
         [Fact]
@@ -36,7 +37,7 @@ namespace NutriFitWebTest.Controllers
 
     ***REMOVED***
 
-        [Fact(Skip = "Doesn't work")]
+        [Fact]
         public void ExercisesController_ShowExerciseList_Should_Return_PartialView()
         ***REMOVED***
             ExercisesController? controller = new ExercisesController(photoManagement);
@@ -55,6 +56,29 @@ namespace NutriFitWebTest.Controllers
             controller.ControllerContext.HttpContext = _httpContext;
 
             IActionResult? result = controller.GetCleanCreateExercisePartial();
+
+            Assert.IsType<PartialViewResult>(result);
+    ***REMOVED***
+
+        [Fact]
+        public void ExercisesController_EditExercise_Should_Return_NotFound()
+        ***REMOVED***
+            ExercisesController? controller = new ExercisesController(photoManagement);
+            controller.ControllerContext.HttpContext = _httpContext;
+
+            IActionResult? result = controller.EditExercise(1);
+
+            Assert.IsType<NotFoundResult>(result);
+    ***REMOVED***
+
+        [Fact]
+        public void ExercisesController_DeleteExercise_Should_Return_PartialView()
+        ***REMOVED***
+            ExercisesController? controller = new ExercisesController(photoManagement);
+            controller.ControllerContext.HttpContext = _httpContext;
+
+
+            IActionResult? result = controller.DeleteExercise(1);
 
             Assert.IsType<PartialViewResult>(result);
     ***REMOVED***
